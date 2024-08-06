bf1bf427b469253441b24a9efeca0e16cb19a776
Nikita Davies <nk548914@dal.ca>
2024-08-06 01:26:44 -0300

    update the backend url

--- frontend/src/api/utils.js
+++ frontend/src/api/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'http://nikita-backend-lb-1481143328.us-east-1.elb.amazonaws.com:8000/api';
+const API_BASE_URL = 'http://Quikbuy-backend-lb-2123908312.us-east-1.elb.amazonaws.com:8000/api';
 
 // Create an instance of Axios with base URL and default headers
 const api = axios.create({
